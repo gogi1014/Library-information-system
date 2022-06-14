8eb228d89785acfa8c7d8fbd598cff1dd084bcbd
gogi1014 <gogi1014@abv.bg>
2022-06-14 13:02:42 +0300

    Create index.php

---
+++ SITE/index.php
@@ -0,0 +1,177 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
+<html>
+<head>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" href="search.css">
+
+
+
+
+<title>1-ва страница</title>
+</head>
+
+<body>
+
+<?php
+    include("reusesearch.php");
+?>
+
+
+
+
+
+<div id="content"  >
+
+<?php if(isset($_COOKIE["username"])) { ?>
+      
+      
+      
+      <form method="post">
+              <input type="submit" name="button1" onclick="grid_view()"
+                      value="Button1"/>
+                
+              <input type="submit" name="button2" onclick="list_view()"
+                      value="Button2"/>
+          </form>
+      
+<?php 
+
+}
+
+
+
+include ('db.php');
+
+
+
+
+if (isset($_GET['pageno'])) {
+  $pageno = $_GET['pageno'];
+} else {
+  $pageno = 1;
+}
+$no_of_records_per_page = 6;
+$offset = ($pageno-1) * $no_of_records_per_page;
+
+
+		function search(string $dbFieldToCheck, $con, $offset, $no_of_records_per_page){
+      $stmt = $con->prepare("SELECT * FROM knigi 
+               Where
+               $dbFieldToCheck LIKE	? 
+      LIMIT  $offset, $no_of_records_per_page");
+      $search = "%".$_GET['search']."%";
+      $stmt->bind_param("s", $search);
+      $stmt->execute();
+      $result = $stmt->get_result();
+      
+
+      if(isset($_COOKIE["username"])) { 
+        if (!isset($_SESSION['View'])) {
+          include ("index_grid.php");
+        }
+        else {
+          include ($_SESSION['View']);
+        }
+          
+          
+       
+      
+           if (isset($_POST['button1'])) {
+                 $_SESSION['View'] = "index_grid.php"; 
+               
+              }
+              elseif (isset($_POST['button2']))
+
+             {
+                 $_SESSION['View'] = "index_table.php"; 
+              }
+      
+     
+    }
+    else {
+      include ("index_grid.php");
+    }
+  }
+    
+
+
+
+
+
+
+if( isset($_GET['search']) && $_GET['search'] != "" ){
+
+    
+    
+    if($_GET['search_type'] == "avtor") {
+      search("AUTHOR", $con, $offset, $no_of_records_per_page);
+      }
+    if($_GET['search_type'] == "ime") {
+      search("IME", $con, $offset, $no_of_records_per_page);
+        }
+      
+}
+else{
+  ?>
+  <script type="text/javascript">$('#content').hide()</script>
+  <?php } ?>
+  </div>
+
+  <?php if( isset($_GET['search']) && $_GET['search'] != "" ){ ?>
+
+  <div class="pagg">   
+
+<?php
+$sql="select * from knigi";
+  $rs_result=mysqli_query($con,$sql);
+  $total_records=mysqli_num_rows($rs_result);
+  $total_pages=ceil($total_records/$no_of_records_per_page);
+  for($i=1;$i<=$total_pages;$i++){
+    echo ($i==$pageno) ? 
+    
+    '<nav aria-label="Page navigation example">
+    <ul class="pagination">
+    <strong><a class="page-link" href="?pageno='.$i.'&search='.$_GET['search'].'&search_type='.$_GET['search_type'].'">'.$i.'</a></strong> </ul>
+    </nav>' : 
+
+    '<nav aria-label="Page navigation example">
+    <ul class="pagination">
+    <a class="page-link" href="?pageno='.$i.'&search='.$_GET['search'].'&search_type='.$_GET['search_type'].'" >'.$i.'</a> </ul>
+    </nav>';
+
+    
+  }
+
+?>
+<?php
+  }
+else{
+  ?>
+  <script type="text/javascript">$('.pagg').hide()</script>
+  <?php } ?>
+
+  
+  
+
+</div>
+
+<footer class="footer">
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    
+  </div>
+</footer>
+
+
+
+</body>
+</html>
